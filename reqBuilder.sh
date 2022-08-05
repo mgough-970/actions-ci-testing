815acc49f2cf43a1bafe2b2ac8889f7e6e6ae47f
Michael Gough <gough@stsci.edu>
2022-08-05 10:47:40 -0400

    Update to req's using auto-gen

--- reqBuilder.sh
+++ reqBuilder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Parsing requirements from ./notebooks
-find ./notebooks -name "*.ipynb" | xargs -I {} pipreqsnb {} > /dev/null 2>&1
+find ./notebooks -name "*.ipynb" | xargs -I {} pipreqsnb {}
 find ./notebooks -name "requirements.txt" | xargs -I {} cat {} >> reqList.txt
 cat reqList.txt | cut -f1 -d"=" | sort -u > reqList2.txt
 ureqs=`cat reqList2.txt | wc -l`
